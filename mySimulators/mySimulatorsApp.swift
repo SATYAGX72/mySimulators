df3419ac31e38bf28a49f9216e2b4318998ff7f7
SATYAGX72 <satya.arja@gmail.com>
2024-09-28 01:03:02 +0530

    Initial Commit

---
+++ mySimulators/mySimulatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mySimulatorsApp.swift
+//  mySimulators
+//
+//  Created by apple on 28/09/24.
+//
+
+import SwiftUI
+
+@main
+struct mySimulatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mySimulatorsTests/mySimulatorsTests.swift
@@ -0,0 +1,16 @@
+//
+//  mySimulatorsTests.swift
+//  mySimulatorsTests
+//
+//  Created by apple on 28/09/24.
+//
+
+import Testing
+
+struct mySimulatorsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
